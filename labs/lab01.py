c63f09ff845c034faade40fd717a6a091a904b22
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-09-22 18:27:08 -0700

    Create lab01.py

---
+++ labs/lab01.py
@@ -0,0 +1,14 @@
+#
+# This is a header block example for lab 1.
+#
+# You will need to supply the following information.
+#
+# Name: Miguel Villa Floran
+# Instructor: Vanessa Rivera
+# Section: 07
+# Why did the programmer quite their job? Because they couldn't get arrays! :)))
+
+import unittest
+
+if __name__ == '__main__':
+  print("Hello, World.")
