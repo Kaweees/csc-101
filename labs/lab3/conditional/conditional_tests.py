0bcca447664db1b3a30c4d3f629a15ef61cd3393
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-09-28 01:35:09 -0700

    Add conditional

--- labs/lab3/conditional/conditional_tests.py
+++ labs/lab3/conditional/conditional_tests.py
@@ -2,12 +2,10 @@ import unittest
 import conditional
 
 class TestCases(unittest.TestCase):
-    def test_case(self):
-        # Replace pass with the test code.
-        pass
-
+  def test_case(self):
+    # Replace pass with the test code.
+    pass
 
 # Run the unit tests.
 if __name__ == '__main__':
-    unittest.main()
-
+  unittest.main()
