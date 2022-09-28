1df8a4cd5f0bfe2f7a617076f2b5c355243dfa70
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-09-28 00:53:19 -0700

    Update point.py

--- labs/lab3/object_equality/point.py
+++ labs/lab3/object_equality/point.py
@@ -1,4 +1,8 @@
+import math
+
 class Point:
-    def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
+  def __init__(self, x: float, y: float):
+    self.x = x
+    self.y = y
+  def __eq__(self, other : object) -> bool:
+    return type(other) is Point and math.isclose(self.x, other.x) and math.isclose(self.y, other.y)
