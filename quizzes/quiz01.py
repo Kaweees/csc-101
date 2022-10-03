37cbfb8fdaf485774b015a57ed807ad0b8c280e2
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-10-03 08:53:49 -0700

    Create quiz01.py

---
+++ quizzes/quiz01.py
@@ -0,0 +1,13 @@
+class ProgrammingLanguage():
+  def __init__(self, name: str, version: int, has_objects: bool):
+    self.name = name
+    self.version = version
+    self.has_objects = has_objects
+
+python = ProgrammingLanguage("Python", 2, True)
+
+def have_same_version(a: ProgrammingLanguage, b: ProgrammingLanguage):
+  return a.version == b.version
+
+python2 = ProgrammingLanguage("Fake Python", 2, False)
+print(have_same_version(python, python2)) # must be True
