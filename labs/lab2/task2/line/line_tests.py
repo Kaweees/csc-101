5d2fa956a50dc9d092a89bb11e3435f9f6d64d7b
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-10-06 20:08:28 -0700

    Update line_tests.py

--- labs/lab2/task2/line/line_tests.py
+++ labs/lab2/task2/line/line_tests.py
@@ -4,7 +4,7 @@ import line
 class LineTests(unittest.TestCase):
   def test_line(self):
     # The following line should show a warning on the value "shoe".
-    result = line.Line("shoe", 2.0, 3.0, 4.0)
+    result = line.Line(1.0, 2.0, 3.0, 4.0)
     self.assertEqual(result.x1, 1.0)
     self.assertEqual(result.y1, 2.0)
     self.assertEqual(result.x2, 3.0)
@@ -12,11 +12,11 @@ class LineTests(unittest.TestCase):
 
   def test_line_again(self):
     # Add code here.
-    l1 = Line(687.0, 254.0, 9999.0, 4414.0)
-    self.assertEqual(result.x1, 687.0)
-    self.assertEqual(result.y1, 254.0)
-    self.assertEqual(result.x2, 9999.0)
-    self.assertEqual(result.y2, 4414.0)
+    l1 = line.Line(687.0, 254.0, 9999.0, 4414.0)
+    self.assertEqual(l1.x1, 687.0)
+    self.assertEqual(l1.y1, 254.0)
+    self.assertEqual(l1.x2, 9999.0)
+    self.assertEqual(l1.y2, 4414.0)
 
 # Run the unit tests.
 if __name__ == '__main__':
