0985aebb3dd73f075ed556853712f919cbf445bc
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-10-10 03:42:14 -0700

    Pset 1 complete

--- psets/pset1/hw1.py
+++ psets/pset1/hw1.py
@@ -4,10 +4,8 @@ class Price:
   def __init__(self, dollars: int, cents: int):
     self.dollars = dollars
     self.cents = cents
-
   def __str__(self):
     return f"${str(self.dollars)}.{str(self.cents)}"
-
   def __eq__(self, other):
     return (other is self or
       type(other) == Price and
@@ -23,7 +21,7 @@ class Point:
     self.y = y
   def __eq__(self, other: object) -> bool:
     return (other is self or
-      type(other) == Rectangle and
+      type(other) == Point and
      math.isclose(self.x, other.x) and math.isclose(self.y, other.y))
 
 class Rectangle:
@@ -46,7 +44,7 @@ class Circle:
 
 def rectangle_perimeter(rect: Rectangle) -> float:
   """Returns the perimeter of a rectangle."""
-  return 2 * (rect.top_left.x - rect.bottom_right.x) + 2 * (rect.top_left.y - rect.bottom_right.y)
+  return 2 * (rect.bottom_right.x - rect.top_left.x) + 2 * (rect.top_left.y - rect.bottom_right.y)
 
 def rectangle_lower_half(rect: Rectangle) -> Rectangle:
   """Returns a new Rectangle object that is the lower half of the given Rectangle object."""
@@ -54,12 +52,15 @@ def rectangle_lower_half(rect: Rectangle) -> Rectangle:
 
 def is_square(rect: Rectangle) -> bool:
   """Returns True if the given Rectangle object is a square."""
-  return (rect.top_left.x - rect.bottom_right.x) == (rect.top_left.y - rect.bottom_right.y)
+  return (rect.bottom_right.x - rect.top_left.x) == (rect.top_left.y - rect.bottom_right.y)
 
 def circle_within_rectangle(circle: Circle, rect: Rectangle) -> bool:
   """Returns True if the given Circle object is within the given Rectangle object."""
-  return (circle.center.x - circle.radius) >= rect.bottom_right.x and (circle.center.x + circle.radius) <= rect.top_left.x and (circle.center.y - circle.radius) >= rect.bottom_right.y and (circle.center.y + circle.radius) <= rect.top_left.y
+  return ((circle.center.x + circle.radius) > rect.top_left.x and
+    (circle.center.y + circle.radius) < rect.top_left.y and
+    (circle.center.x - circle.radius) < rect.bottom_right.x and
+    (circle.center.y - circle.radius) > rect.bottom_right.y)
 
 def circle_bound(rect: Rectangle) -> Circle:
   """Returns a new Circle object that is the smallest circle that bounds the given Rectangle object."""
-  return Circle(Point((rect.top_left.x + rect.bottom_right.x) / 2, (rect.top_left.y + rect.bottom_right.y) / 2), math.sqrt(((rect.top_left.x - rect.bottom_right.x) / 2) ** 2 + ((rect.top_left.y - rect.bottom_right.y) / 2) ** 2))
+  return Circle(Point((rect.top_left.x + rect.bottom_right.x) / 2, (rect.top_left.y + rect.bottom_right.y) / 2), math.sqrt((rect.bottom_right.x - rect.top_left.x) ** 2 + (rect.top_left.y - rect.bottom_right.y) ** 2))
---
+++ psets/pset1/hw1_tests.py
@@ -0,0 +1,37 @@
+import hw1
+import unittest
+
+class TestCases(unittest.TestCase):
+  def test_Price_1(self):
+    self.assertTrue(hw1.add_prices(hw1.Price(101, 1), hw1.Price(100, 1)) == hw1.Price(201, 2))
+    self.assertTrue(hw1.add_prices(hw1.Price(301, 1), hw1.Price(105, 1)) == hw1.Price(406, 2))
+
+  def test_Price_2(self):
+    self.assertTrue(hw1.Rectangle(hw1.Point(0, 1), hw1.Point(1, 0)).bottom_right == hw1.Rectangle(hw1.Point(-1, 4), hw1.Point(1, 0)).bottom_right)
+    self.assertTrue(hw1.Rectangle(hw1.Point(1, 0), hw1.Point(4, -1)).top_left == hw1.Rectangle(hw1.Point(1, 0), hw1.Point(6, -5)).top_left)
+
+  def test_Price_eq_1(self):
+    self.assertEqual(hw1.Price(1, 20), hw1.Price(1, 20))
+    self.assertEqual(hw1.Price(20, 20), hw1.Price(20, 20))
+
+  def test_Rectangle_1(self):
+    self.assertEqual(hw1.rectangle_perimeter(hw1.Rectangle(hw1.Point(0, 1), hw1.Point(1, 0))), 4)
+    self.assertEqual(hw1.rectangle_perimeter(hw1.Rectangle(hw1.Point(0, 0), hw1.Point(1, -1))), 4)
+
+  def test_Rectangle_2(self):
+    self.assertEqual(hw1.rectangle_lower_half(hw1.Rectangle(hw1.Point(0, 1), hw1.Point(1, 0))), hw1.Rectangle(hw1.Point(0, 0.5), hw1.Point(1, 0)))
+    self.assertEqual(hw1.rectangle_lower_half(hw1.Rectangle(hw1.Point(0, 0), hw1.Point(1, -1))), hw1.Rectangle(hw1.Point(0, -0.5), hw1.Point(1, -1)))
+  
+  def test_Rectangle_3(self):
+    self.assertTrue(hw1.is_square(hw1.Rectangle(hw1.Point(0, 1), hw1.Point(1, 0))))
+    self.assertTrue(hw1.is_square(hw1.Rectangle(hw1.Point(0, 0), hw1.Point(1, -1))))
+
+  def test_Rectangle_4(self):
+    self.assertTrue(hw1.circle_within_rectangle(hw1.Circle(hw1.Point(0, 0), 1), hw1.Rectangle(hw1.Point(-4, 4), hw1.Point(4, -4))))
+    self.assertTrue(hw1.circle_within_rectangle(hw1.Circle(hw1.Point(0, 0), 3), hw1.Rectangle(hw1.Point(-5, 5), hw1.Point(5, -5))))
+  
+  def test_Rectangle_5(self):
+    self.assertEqual(hw1.circle_bound(hw1.Rectangle(hw1.Point(-2, 1.5), hw1.Point(2, -1.5))), hw1.Circle(hw1.Point(0, 0), 5))
+    self.assertEqual(hw1.circle_bound(hw1.Rectangle(hw1.Point(-1.5, 2), hw1.Point(1.5, -2))), hw1.Circle(hw1.Point(0, 0), 5))
+if __name__ == '__main__':
+  unittest.main()
