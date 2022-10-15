73706654455a1acde1bdf67c9704958bb5ced03c
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-10-14 20:19:50 -0700

    Lab 4 reformatting

--- labs/lab4/ListFunctions/tests.py
+++ labs/lab4/ListFunctions/tests.py
@@ -10,27 +10,27 @@
 import unittest
 
 class listFunctionTests(unittest.TestCase):
-    # Do not modify these tests
-    def test_includedOne(self):
-        self.assertTrue(functions.areTwoEqual([1, 2, 3, 2], 1, 3))
-        self.assertFalse(functions.areTwoEqual([1, 2, 3, 2], 1, 2))
+  # Do not modify these tests
+  def test_includedOne(self):
+    self.assertTrue(functions.areTwoEqual([1, 2, 3, 2], 1, 3))
+    self.assertFalse(functions.areTwoEqual([1, 2, 3, 2], 1, 2))
 
-    def test_includedTwo(self):
-        self.assertEqual(functions.sumOfThree([1, 2, 3, 2]), 0)
-        self.assertEqual(functions.sumOfThree([1, 2]), 0)
-        self.assertEqual(functions.sumOfThree([1, 2, 3]), 6)
+  def test_includedTwo(self):
+    self.assertEqual(functions.sumOfThree([1, 2, 3, 2]), 0)
+    self.assertEqual(functions.sumOfThree([1, 2]), 0)
+    self.assertEqual(functions.sumOfThree([1, 2, 3]), 6)
 
-    def test_includedThree(self):
-        self.assertEqual(functions.sumOfUpToThree([1, 2, 3, 2]), 6)
-        self.assertEqual(functions.sumOfUpToThree([1, 2]), 3)
-        self.assertEqual(functions.sumOfUpToThree([1, 2, 3]), 6)
+  def test_includedThree(self):
+    self.assertEqual(functions.sumOfUpToThree([1, 2, 3, 2]), 6)
+    self.assertEqual(functions.sumOfUpToThree([1, 2]), 3)
+    self.assertEqual(functions.sumOfUpToThree([1, 2, 3]), 6)
 
-    def test_includedFour(self):
-        self.assertEqual(functions.getFromFirstAsIndex([1, 2, 3, 2]), 2)
-        self.assertEqual(functions.getFromFirstAsIndex([2, 2, 3, 2]), 3)
-        self.assertEqual(functions.getFromFirstAsIndex([-1, 2, 3, 2]), 2)
+  def test_includedFour(self):
+    self.assertEqual(functions.getFromFirstAsIndex([1, 2, 3, 2]), 2)
+    self.assertEqual(functions.getFromFirstAsIndex([2, 2, 3, 2]), 3)
+    self.assertEqual(functions.getFromFirstAsIndex([-1, 2, 3, 2]), 2)
 
-    # Write your tests below
+  # Write your tests below
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
