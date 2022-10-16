41e11a9e49670575abdfc51d751b70a1c2d6e845
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-10-16 03:38:28 -0700

    Update logic_tests.py

--- labs/lab3/logic/logic_tests.py
+++ labs/lab3/logic/logic_tests.py
@@ -13,13 +13,13 @@ import logic
 class TestCases(unittest.TestCase):
   def test_logic_one(self):
     # Replace pass with the test code.
-    self.assertEqualTrue(logic.is_even(2))
-    self.assertEqualTrue(logic.is_even(2))
-    self.assertEqualFalse(logic.is_even(1))
+    self.assertTrue(logic.is_even(2))
+    self.assertTrue(logic.is_even(2))
+    self.assertFalse(logic.is_even(1))
 
   def test_logic_two(self):
-    self.assertEqualTrue(logic.in_an_interval(50.0))
-    self.assertEqualTrue(logic.in_an_interval(13.0))
+    self.assertTrue(logic.in_an_interval(50.0))
+    self.assertTrue(logic.in_an_interval(13.0))
 
 # Run the unit tests.
 if __name__ == '__main__':
