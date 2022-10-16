1e25ac223df8a8079becf40a773a264a3a5bafb4
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-10-16 02:36:01 -0700

    Quiz 4

---
+++ quizzes/quiz4/quiz4.py
@@ -0,0 +1,9 @@
+l = [3.33, 2, True, 4, True, 6, "hi", 8, False, 10]
+result = [item for item in l if item == True]
+print(result)
+
+l = [15.0,3.33,2,True,False]
+
+print(l[3])
+print(l[1 + 1])
+print(l[:2])
