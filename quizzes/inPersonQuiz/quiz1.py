ab668150a3e69d8a7fb06b7ba23714a73d5c4016
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-10-17 08:20:47 -0700

    Create quiz1.py

---
+++ quizzes/inPersonQuiz/quiz1.py
@@ -0,0 +1,15 @@
+a = True
+b = False
+c = b or a and not b
+print(c)
+
+a = [0, 1, 2, 3, 4, 5]
+b = ["apple", "orange", "apple", "orange", "apple", "orange", "apple", "orange"]
+
+print(a[2:4])
+print(a[a[ 2 ] + 1])
+print(b[-1])
+print(b[a[2]])
+
+
+print([num for num in range(0, 9) if num <= 5])
