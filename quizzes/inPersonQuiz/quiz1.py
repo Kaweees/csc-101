2b10ff36836e16a408f5116b6d8fbf602173b441
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-10-17 08:21:03 -0700

    Update quiz1.py

--- quizzes/inPersonQuiz/quiz1.py
+++ quizzes/inPersonQuiz/quiz1.py
@@ -7,7 +7,7 @@ a = [0, 1, 2, 3, 4, 5]
 b = ["apple", "orange", "apple", "orange", "apple", "orange", "apple", "orange"]
 
 print(a[2:4])
-print(a[a[ 2 ] + 1])
+print(a[a[2] + 1])
 print(b[-1])
 print(b[a[2]])
 
