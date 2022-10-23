bffed8178eb3a17911dc2d0eb1163586fc2bc0c3
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-10-22 20:11:38 -0700

    Minor fixes

--- labs/lab5/Fold/functions.py
+++ labs/lab5/Fold/functions.py
@@ -16,27 +16,26 @@ def getMinimumDistance(ls: list[int]) -> int:
   minVal = ls[0]
   for i in range(len(ls)):
     if ls[i] < minVal:
-      minVal = ls[i]
+      minVal = i
   return minVal
 
 def areAllTrue(ls: list[bool]) -> bool:
   """return True if all elements in the input list are True"""
-  if len(ls) == []:
+  if ls == []:
     return None
-  for i in range(len(ls)):
-    if ls[i] == False:
+  for item in ls:
+    if item == False:
       return False
   return True
 
 def getCenterPoint(ls: list[objects.Point]) -> objects.Point:
   """return the center point of the input list of points"""
-  x = 0
-  y = 0
+  x, y = 0, 0
   if len(ls) == []:
     return None
-  for i in range(len(ls)):
-    x += ls[i].x
-    y += ls[i].y
+  for item in ls:
+    x += item.x
+    y += item.y
   return objects.Point(x / len(ls), y / len(ls))
 
 def countLessThanFour(ls: list[str]) -> int:
@@ -44,7 +43,7 @@ def countLessThanFour(ls: list[str]) -> int:
   if len(ls) == []:
     return None
   count = 0
-  for i in range(len(ls)):
-    if len(ls[i]) < 4:
+  for item in ls:
+    if item < 4:
       count += 1
   return count
