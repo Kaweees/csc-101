0ef3c5ce48248eb5425943b8eeec029df201174a
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-10-23 22:07:58 -0700

    3/4 parts done

--- psets/pset3/hw3.py
+++ psets/pset3/hw3.py
@@ -7,7 +7,27 @@
 # Purpose/Assignment: Assignment 3
 #
 
+class Book:
+  """Represents a Book with multiple authors"""
+  def __init__(self, title: str, authors: list[str], year: int) -> None:
+    self.title = title
+    self.authors = authors
+    self.year = year
+  def __repr__(self) -> str:
+    return f"Book({self.title},{self.authors},{self.year})"
+  def __eq__(self, other) -> bool:
+    return (other is self or
+      type(other) == Book and
+      self.title == other.title and
+      self.authors == other.authors and
+      self.year == other.year)
+
+def getBooksByAuthors(books: list[Book], authors: list[str]) -> list[Book]:
+  """Returns a list of books that have the given authors"""
+  return [book for book in books if set(authors).issubset(book.authors)]
+
 class Employee:
+  """Represents an Employee with a name, pay rate, and job code"""
   def __init__(self, name: str, pay_rate: int, job_code: int) -> None:
     self.name = name
     self.pay_rate = pay_rate
@@ -20,3 +40,14 @@ class Employee:
       self.name == other.name and
       self.pay_rate == other.pay_rate and
       self.job_code == other.job_code)
+
+def belowAveragePay(employees: list[Employee], average: float) -> list[Book]:
+  """Returns a list of books that have a pay below the average"""
+  return [employee for employee in employees if employee.pay_rate < average]
+
+def validateRoute(city_links : list[list[str]], route: list[str]) -> bool:
+  """Returns true if the route is valid, false otherwise"""
+  for i in range(len(route) - 1):
+    if not(any(city_link for city_link in city_links if set(sorted(route[i:i+2])).issubset(sorted(city_link)))):
+      return False
+  return True
--- psets/pset3/tests.py
+++ psets/pset3/tests.py
@@ -12,8 +12,22 @@ import math
 import hw3
 
 class AssignmentTests(unittest.TestCase):
-  def test_example(self):
+  def test_part_1(self):
+    self.assertEqual(hw3.getBooksByAuthors([hw3.Book("The Hobbit", ["J.R.R. Tolkien"], 1937)], ["J.R.R. Tolkien"]), [hw3.Book("The Hobbit", ["J.R.R. Tolkien"], 1937)])
+    self.assertEqual(hw3.getBooksByAuthors([hw3.Book("Philosopher's Stone", ["J. K. Rowling"], 1997)], ["J. K. Rowling"]), [hw3.Book("Philosopher's Stone", ["J. K. Rowling"], 1997)])
+  def test_part_2(self):
+    self.assertEqual(hw3.belowAveragePay([hw3.Employee("Bob", 25, 687), hw3.Employee("Peter", 35, 687)], 30), [hw3.Employee("Bob", 25, 687)])
+    self.assertNotEqual(hw3.belowAveragePay([hw3.Employee("Bob", 25, 687), hw3.Employee("Peter", 35, 687)], 30), [hw3.Employee("Peter", 35, 687)])
+  def test_part_3(self):
+    city_links = [
+      ['san luis obispo', 'santa margarita'],
+      ['san luis obispo', 'pismo beach'],
+      ['atascadero', 'santa margarita'],
+      ['atascadero', 'creston']
+    ]
+    self.assertTrue(hw3.validateRoute(city_links, ['san luis obispo', 'santa margarita', 'atascadero']))
+    self.assertFalse(hw3.validateRoute(city_links, ['san luis obispo', 'atascadero']))
+  def test_part_4(self):
     pass
-
 if __name__ == "__main__":
   unittest.main()
