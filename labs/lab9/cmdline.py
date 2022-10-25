b94e9f1f27b1329d592e44c6d6a2e43cdc9313ec
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-10-25 08:50:15 -0700

    Create cmdline.py

---
+++ labs/lab9/cmdline.py
@@ -0,0 +1,14 @@
+#
+# Programmer: Miguel Villa Floran
+# GitHub: Kaweees
+# Date: 10/25/2022
+# Course Name: CSC 101 - Fundamentals of Computer Science
+# Section: Week 09 (Nov 14 - Nov 21)
+# Purpose/Assignment: Lab 9
+#
+
+import sys
+
+if __name__ == '__main__':
+  for i, arg in enumerate(sys.argv):
+    print(i, arg)
